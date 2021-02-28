b281f95ee5ef2197def233874a2c6d8379452f08
Karandeep Singh <skarandeep335@gmail.com>
2021-02-28 12:16:09 -0800

    Added route path for assets page.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route, Switch, Redirect } from "react-router-dom";
 import "./App.css";
+import AssetManager from "./components/assets-manager/assets-manager";
 import Login from "./components/login/login";
 import Register from "./components/register/register";
 
@@ -9,6 +10,7 @@ function App() {
       <Switch>
         <Route path="/login" component={Login} exact />
         <Route path="/register" component={Register} exact />
+        <Route path="/assets" component={AssetManager} exact />
         <Redirect to="/login" />
       </Switch>
     </main>

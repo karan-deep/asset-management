b61d565ce32161a7ecbe2163e12c176a8760b65e
Karandeep Singh <skarandeep335@gmail.com>
2021-02-27 22:56:23 -0800

    Added routes path.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import { Route, Switch, Redirect } from "react-router-dom";
 import "./App.css";
 import Login from "./components/login/login";
+import Register from "./components/register/register";
 
 function App() {
-  return <Login />;
+  return (
+    <main>
+      <Switch>
+        <Route path="/login" component={Login} exact />
+        <Route path="/register" component={Register} exact />
+        <Redirect to="/login" />
+      </Switch>
+    </main>
+  );
 }
 
 export default App;

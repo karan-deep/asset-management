27126644fe0ab85c94480c769de66c480b873ff9
Karandeep Singh <skarandeep335@gmail.com>
2021-02-28 12:15:45 -0800

    Added constructor and reference to the parents constructor (super(props)) and added state object.

--- src/components/assets-manager/assets-editor/assets-editor.js
+++ src/components/assets-manager/assets-editor/assets-editor.js
@@ -1,6 +1,25 @@
 import React, { Component } from "react";
 
 class AssetEditor extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      show: props.show,
+      formData: {
+        name: "",
+        assetTypeId: undefined,
+        price: undefined,
+        description: "",
+        purchaseDate: "",
+      },
+      errors: {
+        name: false,
+        assetTypeId: false,
+        price: false,
+        description: false,
+        purchaseDate: false,
+      },
+    };
   render() {
     return <div></div>;
   }

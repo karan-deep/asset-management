d6a75ccad87d2a20895c6361083f736af569effd
Karandeep Singh <skarandeep335@gmail.com>
2021-02-28 11:32:12 -0800

    Added constructor and reference to the parents constructor (super(props)) and added state object.

--- src/components/assets-manager/assets-manager.js
+++ src/components/assets-manager/assets-manager.js
@@ -1,6 +1,15 @@
 import React, { Component } from "react";
 
 class AssetManager extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      show: false,
+      assets: [],
+      selectedAssetId: undefined,
+      type: "",
+    };
+  }
   render() {
     return <div></div>;
   }

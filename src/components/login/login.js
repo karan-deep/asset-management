e9f971733ee93e93fec7be1bf47809c0be0b8c44
Karandeep Singh <skarandeep335@gmail.com>
2021-02-27 21:25:34 -0800

    Added sign-in form for login page.

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,8 +1,46 @@
 import React, { Component } from "react";
+import "./login.css";
 
 class Login extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div className="limiter col-sm-12">
+        <div className="container-login100">
+          <div className="wrap-login100">
+            <div className="login100-form-title">
+              <span className="login100-form-title-1">Sign In</span>
+            </div>
+            <form className="login100-form">
+              <div className="wrap-input100">
+                <span className="label-input100">Email</span>
+                <input
+                  className="input100"
+                  type="email"
+                  name="email"
+                  placeholder="Enter email"
+                />
+                <span className="focus-input100"></span>
+              </div>
+              <div className="wrap-input100">
+                <span className="label-input100">Password</span>
+                <input
+                  className="input100"
+                  type="password"
+                  name="password"
+                  placeholder="Enter password"
+                />
+                <span className="focus-input100"></span>
+              </div>
+              <div className="container-login100-form-btn mt-3">
+                <button type="submit" className="login100-form-btn">
+                  Login
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 

978b141b049b948821cef91bf873d8fe135a1351
Karandeep Singh <skarandeep335@gmail.com>
2021-02-27 22:32:25 -0800

    Added state object to store property of the register component.

--- src/components/register/register.js
+++ src/components/register/register.js
@@ -3,6 +3,17 @@ import React, { Component } from "react";
 class Register extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      formData: {
+        username: "",
+        password: "",
+        confirmPassword: "",
+      },
+      errors: {
+        username: "",
+        password: "",
+        confirmPassword: "",
+      },
     };
   render() {
     return <div></div>;

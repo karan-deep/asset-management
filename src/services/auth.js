0fc718386f140bfd874cb23bb0cad3bfb4f87afc
Karandeep Singh <skarandeep335@gmail.com>
2021-02-28 18:55:46 -0800

    Added comment for AuthService.

--- src/services/auth.js
+++ src/services/auth.js
@@ -4,9 +4,13 @@ import { environment } from "../environment";
 const AuthService = {
   isLogin: false,
   token: "",
+
+  //Function using axios to make API request to authenticate the user for login that returns a promise
   login: async (requestBody) => {
     return axios.post(environment.apiBasePath + "/authenticate", requestBody);
   },
+
+  //Function using axios to make API request to register the user that returns a promise
   register: async (requestBody) => {
     return axios.post(environment.apiBasePath + "/register", requestBody);
   },
